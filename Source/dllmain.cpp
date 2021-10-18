9cd60a1934f84d283ee07f58c591d4be8c1b34b3
InterwebXplorer <InterwebXplorer@gmail.com>
2021-10-19 12:29:24 +1300

    Update

--- Source/dllmain.cpp
+++ Source/dllmain.cpp
@@ -1,8 +1,8 @@
 //Afinity main
 #include <iostream>
 #include <windows.h>
-//Add more includes here
-#include "offsets.hpp" //Hazedumper must be renamed to this when offsets updated
+#include "pch.h"
+//#include "offsets.hpp" //Hazedumper must be renamed to this when offsets updated
 
 //Fetch display resolution
 const int SCREEN_WIDTH = GetSystemMetrics(SM_CXSCREEN); const int xhairx = SCREEN_WIDTH / 2; //X-Axis (Width)
@@ -15,3 +15,24 @@ HANDLE hProcess;
 uintptr_t moduleBase;
 HDC hdc;
 int closest;
+
+/*
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+*/
