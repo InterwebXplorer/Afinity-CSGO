96b9677ffe5bfbd80831cb0c4f77400f5b7586a4
InterwebXplorer <InterwebXplorer@gmail.com>
2021-11-03 14:07:56 +1300

    Menu update

--- Source/menu.cpp
+++ Source/menu.cpp
@@ -1,4 +1,4 @@
-#include "menu.hpp"
+	#include "menu.hpp"
 #define  NOMINMAX
 #include <Windows.h>
 //#include <chrono>
@@ -48,9 +48,25 @@ Colour select 	- ImGuiEx::ColorEdit3("TITLE", g_Options.NAME_NAME);
 */
 
 /* Legit interactives
-Imgui::Checkbox("Enable", g_Options.enable_legit_aimbot);
-Imgui::SliderFloat("FOV", g_Options.aimbot_fov, 0.0f, 90.0f); //90d aimbot
-ImGui::CHeckbox("Use rectangle FOV", g_Options.aimbot_fov_rectangular);
+Imgui::Checkbox("Enable", g_Options.legit_aim_enable);
+Imgui::SliderFloat("FOV", g_Options.legit_aim_fov, 0.0f, 90.0f); //90d aimbot
+ImGui::Checkbox("Use rectangle FOV", g_Options.legit_aim_rectangular);
+ImGui::Checkbox("Wall penetration", g_Options.legit_aim_wall_penetration);
+ImGui::Checkbox("Smoke check", g_Options.legit_aim_smoke_check);
+ImGui::Checkbox("Flash check", g_Options.legit_aim_flash_check);
+ImGui::Checkbox("Enable backtrack", g_Options.legit_aim_backtrack_enable);
+ImGui::SliderFloat("Backtrack amount(ms)", g_Options.legit_aim_backtrack_amount, 0.0f, 200.0f);
+ImGui::SliderFloat("Speed(X)", g_Options.legit_aim_speed_x, 0.0f, 100.0f);
+ImGui::SliderFloat("Speed(Y)", g_Options.legit_aim_speed_y, 0.0f, 100.0f);
+ImGui::SliderFloat("Strength", g_Options.legit_aim_strength, 0.0f, 100.0f);
+ImGui::SliderFloat("Distant FOV", g_Options.legit_aim_distant_fov, 0.0f, 90.0f);
+ImGui::SliderFloat("Delay(ms)", g_Options.legit_aim_delay, 0.0f, 1000.0f);
+ImGui::Checkbox("Recoil control", g_Options.legit_aim_recoil_control);
+ImGui::SliderFloat("X-axis", g_Options.legit_aim_recoil_control_x, 0.0f, 100.0f);
+ImGui::SliderFloat("Y-axis", g_Options.legit_aim_recoil_control_y, 0.0f, 100.0f);
+ImGui::Checkbox("Magnet", g_Options.legit_aim_magnet);
+ImGui::Checkbox("Aimlock", g_Options.legit_aim_aimlock);
+ImGui::SliderFloat("Hitchance", g_Options.legit_aim_hitchance, 0.0f, 100.0f);
 */
 
 //Incomplete
